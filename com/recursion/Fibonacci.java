71361e38b9c310ad2beb1eb976ba69d9906ac307
fullStackDeveloperToBe <99525378+fullStackDeveloperToBe@users.noreply.github.com>
2022-07-29 16:20:18 +0530

    recursion

--- com/recursion/Fibonacci.java
+++ com/recursion/Fibonacci.java
@@ -3,5 +3,13 @@ package com.recursion;
 public class Fibonacci {
     public static void main(String[] args) {
         System.out.println("Fibonacci");
+        int ans = fibonacci(47);
+        System.out.println(ans);
+    }
+    static int fibonacci(int n) {
+        if(n < 2) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
