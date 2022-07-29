338117f475232e19c4b84e0b4a25d3b38b195971
fullStackDeveloperToBe <99525378+fullStackDeveloperToBe@users.noreply.github.com>
2022-07-29 18:23:14 +0530

    factorial of number

---
+++ com/recursion/Factorial.java
@@ -0,0 +1,22 @@
+package com.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int ans = factorial(5);
+        System.out.println(ans);
+        System.out.println(fact(6));
+    }
+    static int factorial(int n) {
+        int val = 1;
+        for(int i=1;i<=n;i++) {
+            val*=i;
+        }
+        return val;
+    }
+    static int fact(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+}
