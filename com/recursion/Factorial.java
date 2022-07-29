2f5f2500a9b3357784c7650abcb85a2305456e5a
fullStackDeveloperToBe <99525378+fullStackDeveloperToBe@users.noreply.github.com>
2022-07-29 18:23:59 +0530

    factorial of number

--- com/recursion/Factorial.java
+++ com/recursion/Factorial.java
@@ -14,7 +14,7 @@ public class Factorial {
         return val;
     }
     static int fact(int n) {
-        if(n == 0 || n == 1) {
+        if(n <= 1) {
             return 1;
         }
         return n * fact(n-1);
